a6a7863bda4a3127a7883a744783e178fe45dfb0
Anthony <AnthonyReid99@gmail.com>
2014-07-23 22:40:16 +0200

    all good

--- EmailReceiver.py
+++ EmailReceiver.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import imaplib
 import getpass
@@ -5,7 +6,7 @@ import email
 import datetime
 import re
 
-EMAIL_ADDRESS = "whereis.dimagi001@gmail.com"
+EMAIL_ADDRESS = "whereis.dimagi002@gmail.com"
 
 
 def receive_emails():
@@ -34,13 +35,38 @@ def receive_emails():
 
     # Are there new emails?
     if not email_ids[0]:
+        print "no email :("
         return
 
     # Parse them
+    email_data = []
     for email_id in email_ids[0].split():
+        print "email!"
         ret, email_string = imap_connection.fetch(str(email_id), '(RFC822)')
         email_contents = email.message_from_string(email_string[0][1])
-        body_text = email_contents.get_payload()[ 0 ].get_payload()
+        email_date = str(datetime.datetime.utcnow())
+        email_from = email.utils.parseaddr(email_contents['From'])
+        email_data.append(
+            (email_date,
+             email_from,
+             email_contents.get_payload()[0].get_payload())
+        )
 
-if __name__ == "__main__":
-    receive_emails()
+    return email_data
+
+
+def dummy_email_receiver():
+    """
+    Generate fake test emails
+    """
+    dummy_data = [["Anthony@gmail.com", "2011-05-19 14:05", "Boston"], ['dskjfhasd']]
+    return dummy_data
+
+
+def parse_email(email):
+    """
+    Parse the email body's JSON
+    """
+
+    return json.loads(email)
+
